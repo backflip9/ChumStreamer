fb5116ead8254009de64ac196fb6934ea69048ba
backflip9 <liam.schroth@gmail.com>
2019-02-16 16:58:12 -0800

    wipe

--- chumlistitem.cpp
+++
@@ -1,7 +0,0 @@
-#include "chumlistitem.h"
-
-//ChumListItem::ChumListItem(QString txt,QListWidget *parent,QString artist) : QListWidgetItem(txt,parent), artistID(artist){}
-/*
-QString ChumListItem::getArtistID(){return artistID;}
-void ChumListItem::setArtistID(QString in){artistID=in;}
-*/
